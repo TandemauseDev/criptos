0e9909959d197f46179a63de47b8880b5561a128
TandemauseDev <152470492+TandemauseDev@users.noreply.github.com>
2023-12-05 20:29:37 -0500

    Update monedas.js

--- src/data/monedas.js
+++ src/data/monedas.js
@@ -1,11 +1,11 @@
     const monedas = [
         {
-            id: 'usd', nombre: 'Dolar de estados unidos' 
+            id: 'USD', nombre: 'Dolar de estados unidos' 
         },
             {id: 'MXN', nombre: 'Peso Mexicano'},
             {id: 'EUR', nombre: 'Euro'},
             {id: 'GBP', nombre: 'Libra Esterlina'},
-           { id: 'COP', nombre: 'Peso Colombiano'},
+
         
     ]
 
